af55eb7f1215973c70bdc5a56828634ce4f9625d
José Carlos Soares <JcSoares29>
2020-03-26 13:06:56 -0300

    Mobile da aplicação

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -26,7 +26,7 @@ export default function NewIncident() {
         }
 
         try {
-            await api.post('incidents',data, {
+            await api.post('incidents', data, {
                 headers: {
                     Authorization: ongId
                 }
